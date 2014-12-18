39e711a12aad119f04b2aaf83b8876f5c1db6e7e
Matthew Ahlberg <matthewahlberg@Matthews-MacBook-Air.local>
2014-12-18 11:13:46 -0800

    more heroku and S3

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,15 +4,21 @@ class Pet < ActiveRecord::Base
   # belongs_to :breed
 
   has_attached_file :avatar, 
-            
+              :storage => :s3,
+              :bucket => 'cutesy',
+              :path => "/image/:id/:filename",
+              :s3_credentials => {
+                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+              },
               :styles => {
               :original => '1920x1680>',
               :small    => '100x100#',
               :medium   => '250x250',
               :large    => '500x500>'
               },
-              :use_timestamp => false,
-              :path => "cutesy/:attachment/:style/:id.:extension"
+              :use_timestamp => false
+              
 
   
    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
@@ -24,4 +30,10 @@ end
 
 
 
-
+:storage => :s3,
+    :bucket => 'mybucket',
+    :path => "/image/:id/:filename",
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
