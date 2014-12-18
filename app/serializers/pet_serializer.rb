c2a1e4beea279855ff59778a806c770c0bf47a33
meidlin <mikeeidlin@gmail.com>
2014-12-17 21:08:38 -0800

    more stuff with mel

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,5 +1,5 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id, :breed_id, :age, :sex, :description, :created_at, :updated_at, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at, :breedname, :ownername, :petavatar
+  attributes :id, :name, :user_id, :breed_id, :age, :sex, :description, :created_at, :updated_at, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at, :breedname, :ownername, :petavatar, :petgender
 
   def breedname
   	object.breed.name
@@ -12,6 +12,10 @@ class PetSerializer < ActiveModel::Serializer
   def petavatar
   	object.avatar.url(:large)
   end
+
+  def petgender
+  	if object.sex == 1 then "M" else "F" end
+  end	
 
 
 
