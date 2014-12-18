114b2a895fff8b3c31e734b757b95907d955178a
Matthew Ahlberg <matthewahlberg@Matthews-MacBook-Air.local>
2014-12-18 12:41:02 -0800

    heroku crazinessss

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,20 +4,20 @@ class Pet < ActiveRecord::Base
  
 
   has_attached_file :avatar, 
-              :storage => :s3,
-              :bucket => 'cutesy',
-              #:path => "/image/:id/:filename",
-              :s3_credentials => {
-                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-              },
-              :styles => {
-              :original => '1920x1680>',
-              :small    => '100x100#',
-              :medium   => '250x250',
-              :large    => '500x500>'
-              },
-              :use_timestamp => false
+                    :storage => :s3,
+                    :bucket => 'cutesy',
+                    #:path => "/image/:id/:filename",
+                    :s3_credentials => {
+                      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                    },
+                    :styles => {
+                    :original => '1920x1680>',
+                    :small    => '100x100#',
+                    :medium   => '250x250',
+                    :large    => '500x500>'
+                    },
+                    :use_timestamp => false
               
 
   
