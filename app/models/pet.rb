4e1b02799c1282074956060688b18946932c482f
meidlin <mikeeidlin@gmail.com>
2014-12-18 11:20:55 -0800

    S3 and Heroku

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,6 +7,10 @@ class Pet < ActiveRecord::Base
               :storage => :s3,
               :bucket => 'cutesy',
               :path => "/image/:id/:filename",
+              :s3_credentials => {
+                :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+              },
               :styles => {
               :original => '1920x1680>',
               :small    => '100x100#',
@@ -24,12 +28,3 @@ class Pet < ActiveRecord::Base
 
 end
 
-
-
-:storage => :s3,
-    :bucket => 'mybucket',
-    :path => "/image/:id/:filename",
-    :s3_credentials => {
-      :access_key_id => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
-    }
